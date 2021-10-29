56425d2c913672f564ee046187315708c97d9377
ttuan <tuantv.nhnd@gmail.com>
2021-10-29 14:05:08 +0700

    Add redirect page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
   def index
   end
+
+  def redirect
+    @state = params[:state]
+    @code = params[:code]
+    @id_token = params[:id_token]
+    @user = parmas[:user]
+
+    # TODO: Validation
+
+    render 'redirect'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "top#index"
+  get "/redirect", to: "top#redirect"
 end
